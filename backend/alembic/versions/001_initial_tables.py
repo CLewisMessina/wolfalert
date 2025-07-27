74c304001ff7cd04afa3cdfb04113da7dde7965d
C Lewis Messina <33812538+CLewisMessina@users.noreply.github.com>
2025-07-27 18:39:13 -0500

    continued 001_initial_tables.py debug

--- backend/alembic/versions/001_initial_tables.py
+++ backend/alembic/versions/001_initial_tables.py
@@ -8,7 +8,7 @@ Create Date: 2025-07-25 00:00:00.000000
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+# from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '001_initial'
